a2421e2a534f6a670d85368d00f5e922878e909a
MoNabawy-2003 <naboy22446688@gmail.com>
2025-10-06 22:55:45 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/al-safaa-hospital/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      base: '/al-safaa-hospital/',
+      
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
